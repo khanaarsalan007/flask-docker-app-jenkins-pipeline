47aa0e626116a23a30eda31bea3555ac7a72b921
Arsalan Khan <57859640+khanaarsalan007@users.noreply.github.com>
2020-06-05 00:30:28 +0500

    changes in app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():    
-    return 'Welcome to the Application!'
+    return "Welcome to the Arsalan's Application!"
 
 @app.route('/stub')
 def stub():
